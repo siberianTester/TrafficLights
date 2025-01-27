ecd6078a7ff5de5b96bd574022d522159e1bfbee
Yaroslav Malygin <ya.malygin@team.superjob.ru>
2025-01-27 20:20:40 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Yaroslav Malygin on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrafficLightTests/TrafficLightTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightTests.swift
+//  TrafficLightTests
+//
+//  Created by Yaroslav Malygin on 27.01.2025.
+//
+
+import Testing
+@testable import TrafficLight
+
+struct TrafficLightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
